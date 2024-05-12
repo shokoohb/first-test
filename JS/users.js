04b6855a556037ac5797b9dc0a909d2b023c131a
shokoohb <shokoohbehzadfar@yahoo.com>
2024-05-12 19:46:52 +0330

    5th commit

--- JS/users.js
+++ JS/users.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+
+
+
+    const addUserbutton = document.getElementById('add_user_button')
+    addUserbutton.addEventListener('click', ()=>{
+        document.querySelector('.add_user_modal').classList.add('show')
+        document.querySelector('.add_user_modal_back').classList.remove('dis-none')
+    })
+
+    document.querySelector('.add_user_modal_back').addEventListener('click',(e)=>{
+        document.querySelector('.add_user_modal').classList.remove('show')
+        e.target.classList.add('dis-none')
+    })
 });
