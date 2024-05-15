1d8a5915e2b806c2c3e6133f8b2dc595b30c2e8f
shokoohb <shokoohbehzadfar@yahoo.com>
2024-05-15 14:42:41 +0330

    6th commit

--- JS/main.js
+++ JS/main.js
@@ -3,4 +3,39 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems, {
       edge: "right"
     });
-  });
+
+
+
+
+// Promise.......................
+  // console.log(1);
+
+  // new Promise(
+  //   function(resolve,reject){
+  //     setTimeout(function(){
+  //       console.log(2);
+  //       resolve("ok")
+  //       // reject("not ok...")
+  //     },1000)
+  //   }
+  // ).then(
+  //   function(response){
+  //     console.log(response);
+  //     console.log(3);
+  //   }
+  // ).catch(
+  //   function(error){
+  //     console.log(error);
+  //   }
+  // )
+
+
+  // console.log(4);
+  // console.log(5);
+
+
+// restful...........................
+//     fetch("https://jsonplaceholder.typicode.com/posts/1")
+//       .then(Response) => Response.json())
+//       .then(json) => console.log(json));
+});
